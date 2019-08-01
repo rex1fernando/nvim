191d04b1452f51aba310b8f8a6c5bc7b4a47d794
Rex Fernando <rex1fernando@gmail.com>
2019-08-01 14:47:14 +0300

    python 3

--- rplugin/python/tandem_lib/agent/tandem/shared/utils/proxy.py
+++
@@ -1,7 +0,0 @@
-class ProxyUtils(object):
-    @staticmethod
-    def run(proxies, method, data):
-        for proxy in proxies:
-            data = getattr(proxy, method, lambda x: x)(data)
-
-        return data
